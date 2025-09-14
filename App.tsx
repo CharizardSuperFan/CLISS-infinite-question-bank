4b3e28a22dc6a8860d5b775b99514595f3dbaa41
CharizardSuperFan <SuperCharizardFan@gmail.com>
2025-09-14 11:57:55 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Question, Option } from './types';
-import { GenerateIcon, BankIcon, HistoryIcon, CheckIcon, XIcon, AlertIcon, StarIcon, StarOutlineIcon, EyeSlashIcon } from './components/IconComponents';
+import { GenerateIcon, BankIcon, HistoryIcon, CheckIcon, XIcon, AlertIcon, StarIcon, StarOutlineIcon, EyeSlashIcon } from './components/IconComponents.tsx';
 
 type ActiveTab = 'generate' | 'bank' | 'history';
 
@@ -662,3 +662,4 @@ const App: React.FC = () => {
 };
 
 export default App;
+
